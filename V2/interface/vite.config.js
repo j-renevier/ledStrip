29d5b98c424608a8d5840b9e7e386e8944157629
Joachim Renevier <renevierjoachim@gmail.com>
2025-06-14 11:00:22 +0200

    change buiild

--- V2/interface/vite.config.js
+++ V2/interface/vite.config.js
@@ -4,8 +4,10 @@ import preact from '@preact/preset-vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 const isArduinoBuild = process.env.ARDUINO_BUILD === 'true'
+const base = process.env.VITE_BASE_PATH || '/'
 
 export default defineConfig({
+  base,
   plugins: [
     preact(),
     VitePWA({
