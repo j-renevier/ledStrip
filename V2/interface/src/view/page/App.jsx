b795eb8728413dff3d6ec5b5f9439a263244344f
Joachim Renevier <renevierjoachim@gmail.com>
2025-06-14 14:11:04 +0200

    test 72

--- V2/interface/src/view/page/App.jsx
+++ V2/interface/src/view/page/App.jsx
@@ -18,8 +18,8 @@ const App = () => {
 
   return(
     <Router>
-      <Dashboard path="/ledStrip/" protocole={protocole} host={host} setHost={setHost} port={port} root={root}/>
-      <Home path="/ledStrip/home" protocole={protocole} host={host} setHost={setHost} port={port} root={root}/>
+      <Dashboard path="ledStrip/" protocole={protocole} host={host} setHost={setHost} port={port} root={root}/>
+      <Home path="ledStrip/home" protocole={protocole} host={host} setHost={setHost} port={port} root={root}/>
 
     </Router>
   )
