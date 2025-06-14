7470520768fc030520c509306a1306eb10400c46
Joachim Renevier <renevierjoachim@gmail.com>
2025-06-14 11:55:10 +0200

    relative

--- V2/interface/vite.config.js
+++ V2/interface/vite.config.js
@@ -4,13 +4,11 @@ import preact from '@preact/preset-vite'
 
 // Mode d'environnement
 const isArduinoBuild = process.env.ARDUINO_BUILD === 'true'
-const base = process.env.VITE_BASE_PATH || '/'
 const outDir = isArduinoBuild
   ? path.resolve(__dirname, '../arduino/data')
   : 'dist'
 
 export default defineConfig({
-  base,
   plugins: [
     preact(),
   ],
